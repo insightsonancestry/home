e63c2736dac1ba3e0550c3f49cc238b14aa0790b
SahilArvind <sahil.23fe10ite00234.muj.manipal.edu>
2025-07-20 18:49:14 +0530

    added favicon

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,18 +1,18 @@
+"use client";
 import Image from 'next/image';
 import admixturemodelling from '../assets/images/admixture_modelling.png';
 import PCA from '../assets/images/pca.png';
 import YDNA from '../assets/images/ydna.png';
 import customdataset from '../assets/images/customdataset.png';
 import rawfileconversion from '../assets/images/rawfile.png';
-import extractingrawfiles from '../assets/images/extracting-rawfiles.png';
+import {motion} from "framer-motion";
 
 const images = [
-  { src: admixturemodelling, alt: "admixture modelling text" },
-  { src: PCA, alt: "PCA" },
-  { src: YDNA, alt: "Y-DNA haplogroup" },
-  { src: customdataset, alt: "custom dataset" },
-  { src: rawfileconversion, alt: "raw file conversion" },
-  { src: extractingrawfiles, alt: "extracting raw files of ancient DNA samples" }
+  {src: admixturemodelling, alt:'admixture modelling'}, 
+  {src: PCA, alt:'Principal Component Analysis'}, 
+  {src: YDNA, alt:'Y-chromosome haplogroup'}, 
+  {src: customdataset, alt:'curating custom dataset'}, 
+  {src: rawfileconversion, alt:'converting raw file format to 23andme'}, 
 ];
 
 export const LogoTicker = () => {
@@ -20,17 +20,23 @@ export const LogoTicker = () => {
     <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
         <h2 className="text-xl text-center text-white/70">Services that we offer</h2>
-        <div className="overflow-hidden mt-9 before:content-[''] after:content[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
-          <div className="flex gap-16 justify-center items-center">
-          {images.map(({src, alt}) => (
-            <Image 
-              src={src} 
-              alt={alt}
-              key={alt}
-              className="flex-none h-8 w-auto"
-            />
+        <div className="flex overflow-hidden mt-9 before:content-[''] before:z-10 after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0)] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0)]">
+          <motion.div 
+                      transition={{
+                        duration: 10,
+                        ease: "linear",
+                        repeat: Infinity,
+                      }}
+                      initial={{translateX: 0}}
+                      animate={{translateX: "-50%"}}
+                      className="flex gap-16 flex-none pr-16">
+          {images.map(({ src, alt }) => (
+            <Image key={alt} src={src} alt={alt} className="flex-none  h-10 w-2000" />
           ))}
-          </div>
+          {images.map(({ src, alt }) => (
+            <Image key={alt} src={src} alt={alt} className="flex-none  h-10 w-2000" />
+          ))}
+          </motion.div>
         </div>
       </div>
     </div>
